c422a33c767d942f8221a1c45a9ce1362f69f56b
Rahul Tripathi <rt84153@gmail.com>
2024-09-14 10:45:24 +0530

    Intial commit for NextJS App

---
+++ src/pages/about/index.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function About() {
+  return (
+    <div>
+      <h1>Hello, Next JS About</h1>
+      <Link href={"/"}>Go Back</Link>
+    </div>
+  );
+}
